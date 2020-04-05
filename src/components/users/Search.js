5c73112d21e0e8dbc10b096200dcd6bd042a6ef8
pjmantoss <joelptoss@gmail.com>
2020-04-05 20:20:17 +0100

    update Search.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -37,7 +37,7 @@ const Search = () => {
                </form> 
                {githubContext.users.length > 0 && (
                   <button className="btn btn-light btn-block" onClick={githubContext.clearUsers}>
-                  clear
+                  Clear
                   </button>
                )}
                
